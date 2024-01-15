489d844db84cc125a8a0c91e3994a822b3445b17
SamuelKamothoMaingi <SamuelKamothoMaingi@DESKTOP-V8VDAUT>
2024-01-15 14:59:42 +0300

    Add project files.

---
+++ BlazorApp2/Model/Booking.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorWeb.Model
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int Price { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public string image { get; set; } = string.Empty;
+    }
+}
